d7b7b0a09bd76e7505119bf277c358d1469a594c
TizianoN <hello@tizianonicolai.com>
2024-12-04 11:00:39 +0100

    feat: edit form

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,148 @@
-// import { useState } from 'react'
+import { useState } from 'react';
 
 function App() {
+  const [titleFieldInsert, setTitleFieldInsert] = useState('');
+  const [titleFieldEdit, setTitleFieldEdit] = useState('');
+  const [articleList, setArticleList] = useState([]);
+  const [postEditIndex, setPostEditIndex] = useState();
+
+  // # INSERT POST
+  const handleInsertPostSubmit = (e) => {
+    e.preventDefault();
+
+    // console.log(titleField);
+    // console.log(articleList);
+
+    if (!titleFieldInsert) return;
+
+    const newArticle = {
+      title: titleFieldInsert,
+    };
+
+    setArticleList([...articleList, newArticle]);
+    setTitleFieldInsert('');
+  };
+
+  const handleInsertTitleChange = (e) => {
+    setTitleFieldInsert(e.target.value);
+  };
+
+  // # EDIT POST
+  const handleEditPostSubmit = (e) => {
+    e.preventDefault();
+
+    console.log(titleFieldEdit);
+    console.log(postEditIndex);
+
+    if (!titleFieldEdit) return;
+    if (!postEditIndex) return;
+
+    const newArticleList = [...articleList];
+    newArticleList[postEditIndex].title = titleFieldEdit;
+    setArticleList(newArticleList);
+  };
+
+  const handleChangePostEdit = (e) => {
+    setPostEditIndex(e.target.value);
+  };
+
+  const handleEditTitleChange = (e) => {
+    setTitleFieldEdit(e.target.value);
+  };
+
+  // # DELETE POST
+  const deletePost = (deleteIndex) => {
+    const newArticleList = articleList.filter((article, articleIndex) => articleIndex !== deleteIndex);
+    setArticleList(newArticleList);
+  };
+
   return (
     <>
       <div className="container">
-        <h1 className="mt-5">Hello World</h1>
+        {/* INSERT POST FORM SECTION */}
+        <section className="py-4">
+          <form onSubmit={handleInsertPostSubmit}>
+            <h2>Inserisci nuovo post</h2>
+            <div className="row">
+              <div className="col-3">
+                <label className="form-label" htmlFor="post-title">
+                  Titolo
+                </label>
+                <input
+                  value={titleFieldInsert}
+                  onChange={handleInsertTitleChange}
+                  type="text"
+                  className="form-control mb-3"
+                  id="post-title"
+                />
+              </div>
+
+              <div className="col-12">
+                <button className="btn btn-success">Crea post</button>
+              </div>
+            </div>
+          </form>
+        </section>
+        <hr />
+        {/* EDIT POST FORM SECTION */}
+        <section className="py-4">
+          <form onSubmit={handleEditPostSubmit}>
+            <h2>Modifica post</h2>
+            <div className="row">
+              <div className="col-3">
+                <label className="form-label" htmlFor="post-selection">
+                  Seleziona post
+                </label>
+                <select onChange={handleChangePostEdit} className="form-select" id="post-selection">
+                  <option value="">Seleziona un post</option>
+                  {articleList.map((article, index) => (
+                    <option value={index}>{article.title}</option>
+                  ))}
+                </select>
+              </div>
+
+              <div className="col-3">
+                <label className="form-label" htmlFor="post-title">
+                  Titolo
+                </label>
+                <input
+                  value={titleFieldEdit}
+                  onChange={handleEditTitleChange}
+                  type="text"
+                  className="form-control mb-3"
+                  id="post-title"
+                />
+              </div>
+
+              <div className="col-12">
+                <button className="btn btn-success">Modifica</button>
+              </div>
+            </div>
+          </form>
+        </section>
+        <hr />
+        {/* LIST POST SECTION  */}
+        <section className="py-4">
+          <h2>Post list</h2>
+          <div className="row row-cols-3 g-3">
+            {articleList.length ? (
+              articleList.map((article, index) => (
+                <div key={index} className="col">
+                  <div className="card">
+                    <button onClick={() => deletePost(index)} type="button" className="btn-close"></button>
+                    <div className="card-body">
+                      <h3>{article.title}</h3>
+                    </div>
+                  </div>
+                </div>
+              ))
+            ) : (
+              <div className="col-12">
+                <h3>Nessun articolo disponibile</h3>
+              </div>
+            )}
+          </div>
+        </section>
       </div>
     </>
   );
