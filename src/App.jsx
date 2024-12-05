a9e140bbf9472023bc69f2098c3c665263ff4180
TizianoN <hello@tizianonicolai.com>
2024-12-05 10:22:37 +0100

    Revert "fix: return to only insert form"
    This reverts commit 994c85e29fddb07ca77e5197422d1960ec2d781e.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,5 @@
 import { useState } from 'react';
 
-const possibleTags = ['HTML', 'CSS', 'JS'];
-
-const defaultPostFields = {
-  title: '',
-  author: '',
-  description: '',
-  published: false,
-  tags: [],
-};
-
 function App() {
   const [titleFieldInsert, setTitleFieldInsert] = useState('');
   const [titleFieldEdit, setTitleFieldEdit] = useState('');
@@ -84,75 +74,11 @@ function App() {
                   type="text"
                   className="form-control mb-3"
                   id="post-title"
-                  name="title"
                 />
-              </div>
-
-              <div className="col-3">
-                <label className="form-label" htmlFor="post-author">
-                  Autore
-                </label>
-                <input
-                  value={formFields.author}
-                  onChange={handleFormChange}
-                  type="text"
-                  className="form-control mb-3"
-                  id="post-author"
-                  name="author"
-                />
-              </div>
-
-              <div className="col-3">
-                <label className="form-label" htmlFor="post-description">
-                  Description
-                </label>
-                <input
-                  value={formFields.description}
-                  onChange={handleFormChange}
-                  type="text"
-                  className="form-control mb-3"
-                  id="post-description"
-                  name="description"
-                />
-              </div>
-
-              <div className="col-3">
-                <label className="form-label" htmlFor="post-published">
-                  Pubblicato
-                </label>
-                <div>
-                  <input
-                    checked={formFields.published}
-                    onChange={handleFormChange}
-                    type="checkbox"
-                    id="post-published"
-                    name="published"
-                  />
-                </div>
-              </div>
-
-              <div className="col-3">
-                <label className="form-label">Tags</label>
-
-                {possibleTags.map((tag, index) => (
-                  <div key={index}>
-                    <label>
-                      <input
-                        checked={formFields.tags.includes(tag)}
-                        onChange={handleFormTagsChange}
-                        type="checkbox"
-                        name="post-tag"
-                        value={tag}
-                        className="me-2"
-                      />
-                      {tag}
-                    </label>
-                  </div>
-                ))}
               </div>
 
               <div className="col-12">
-                <button className="btn btn-success">Crea post</button>
+                <button className="btn btn-success">Modifica</button>
               </div>
             </div>
           </form>
